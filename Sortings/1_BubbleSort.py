a88dff07fbdf8c2c17760412b9f2c80e0291b181
Elena <84009999+Elen4538@users.noreply.github.com>
2021-11-05 22:52:38 +0300

    Update 1_BubbleSort.py

--- Sortings/1_BubbleSort.py
+++ Sortings/1_BubbleSort.py
@@ -1,19 +1,11 @@
-
-#по убыванию
-def BubbleSort(A): 
-    for j in range(len(A) - 1, 0, -1): 
-        for i in range(0, j): 
-            if A[i] > A[i + 1]: 
-                A[i], A[i + 1] = A[i + 1], A[i]
-                
-                
-         # по возрастанию       
+# по возрастанию       
 def bubbleSort(arr): 
     n = len(arr)    
     for i in range(n):          
         for j in range(0, n-i-1): 
-            if arr[j] > arr[j+1] : 
+            if arr[j] > arr[j+1] :  # если поменять знак будет по убыванию
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
+    return arr
                 
                 
 arr=[int(i) for i in input().split()]
