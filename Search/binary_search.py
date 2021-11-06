c9ad657a5040b46d8522621f474d3cc9adfef5e8
Elena <84009999+Elen4538@users.noreply.github.com>
2021-11-06 12:59:41 +0300

    Create binary_search.py

---
+++ Search/binary_search.py
@@ -0,0 +1,25 @@
+
+def binarySearch(alist, item):
+	    first = 0 #индекс первого элемента
+	    last = len(alist)-1#индекс последнего элемента
+	    found = False
+	
+	    while first<=last and not found:#индексы
+	        midpoint = (first + last)//2 #ищем середину списка- индекс
+	        if alist[midpoint] == item:# если средний элемент в списке равен искомому то ОК
+	            found = True
+	        else:
+	            if item < alist[midpoint]:#если искомое меньше среднего элемента
+	                last = midpoint-1 # то сдвигаем границу поиска
+	            else:
+	                first = midpoint+1#наоборот то в правой 
+	
+	    return found
+	
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(binarySearch(testlist, 3))
+print(binarySearch(testlist, 13))
+ # Step 1 of 42  
+ #line that has just executed
+ #время работы O(logN)
+ 
