e5361b541513abb626a1e59b60d2ef793f87de34
Elena <84009999+Elen4538@users.noreply.github.com>
2021-11-06 12:21:07 +0300

    Update 7_simple_counting_sort.py

--- Sortings/7_simple_counting_sort.py
+++ Sortings/7_simple_counting_sort.py
@@ -1,35 +1,7 @@
 #сортировка подсчетом
-def maxi(A):
-    maximus=0
-    for i in range(0,len(A)-1):   # функция максимум
-        for i in range(i,len(A)-1):
-            if A[i] > A[i+1]:
-                maximus=A[i]
 
-    return(maximus)
------------------------------------------------------------------
-def SimpleCountingSort(A):
-    
-    size = maxi(A) + 1    с функцией максимум
-    C = [0] * size
-    for x in A:
-        C[x] += 1
-    A[:] = []
-    for number in range(size):
-        A += [number] * C[number] 
-        
-        
-    return A
-        
-        
-A=[int(s) for s in input().split()]
-
-SimpleCountingSort(A)
-print(*A)
-
---------------------------------------
 def SimpleCountingSort(A):   # без функции максимум
-    scope = max(A) + 1  # O(n+k)   линейное время в лечшем случае O(n)
+    scope = max(A) + 1       # O(n+k)   линейное время в лечшем случае O(n)
     C = [0] * scope
     for x in A :
         C[x] += 1
