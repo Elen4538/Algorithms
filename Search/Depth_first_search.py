17141a732c45094cc5ce3f65e43ba2bc05d5860a
Elena <84009999+Elen4538@users.noreply.github.com>
2022-07-26 12:31:05 +0300

    Create Depth_first_search.py

---
+++ Search/Depth_first_search.py
@@ -0,0 +1,14 @@
+visited = [False] * (n + 1)
+prev = [None] * (n + 1)
+
+
+
+def dfs(start, visited, prev, g):
+    visited[start] = True
+    for u in g[start]:
+        if not visited[u]:
+            prev[u] = start 
+            dfs(u)
+
+            
+ dfs(start, visited, prev, g)
